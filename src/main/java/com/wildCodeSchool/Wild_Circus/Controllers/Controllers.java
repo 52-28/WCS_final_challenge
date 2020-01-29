395df0dec44dd4059bf06dfb82750e1534d1a0e3
52-28 <aymeric.feillault@gmail.com>
2020-01-29 16:51:34 +0100

    ajout carousel dynamique

--- src/main/java/com/wildCodeSchool/Wild_Circus/Controllers/Controllers.java
+++ src/main/java/com/wildCodeSchool/Wild_Circus/Controllers/Controllers.java
@@ -1,15 +1,28 @@
 package com.wildCodeSchool.Wild_Circus.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wildCodeSchool.Wild_Circus.services.ImodelServices;
+import com.wildCodeSchool.Wild_Circus.services.ModelServices;
+
 @Controller
 public class Controllers {
 
+	@Autowired
+	ImodelServices modelServices;
+	
 	@GetMapping("/admin")
 	public ModelAndView getAdmin() {
 		return new ModelAndView("admin");
+	}
+
+	@GetMapping("/")
+	public ModelAndView gethome() {
+		
+		return modelServices.getHomeModel();
 	}
 	
 	@GetMapping("/admin/carousel")
